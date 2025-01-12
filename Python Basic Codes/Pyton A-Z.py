a179bd1856c25173c4be89de8ed451ae31a1850d
Adithi <aj301574@falmouth.ac.uk>
2025-01-12 17:47:41 +0000

    Feat: Uploding the Python Basic codes

---
+++ Python Basic Codes/Pyton A-Z.py
@@ -0,0 +1,14 @@
+#variable = a container for a value. Behaves as the value that it contains 
+#string = "x" or 'x' contains letters
+#first_name = "Bro"
+#last_name = "Code"
+#Full_name = first_name + last_name
+#print("Hello " + Full_name)
+#print(type(name))
+
+#Interger string
+age = 21
+age = age + 1
+print(age)
+print(type(age))
+
